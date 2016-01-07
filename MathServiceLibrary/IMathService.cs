fd81b997ed7de183697f723ff72a7b5ebbfed997
knolegsb <seon2you@hotmail.com>
2016-01-06 17:56:39 -0800

    sevice library

---
+++ MathServiceLibrary/IMathService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace MathServiceLibrary
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IMathService
+    {
+        [OperationContract]
+        int Add(MyNumbers obj);
+        [OperationContract]
+        int Subtract(MyNumbers obj);
+        //[OperationContract]
+        //string GetData(int value);
+
+        //[OperationContract]
+        //CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        //// TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "MathServiceLibrary.ContractType".
+    [DataContract]
+    public class MyNumbers
+    {
+        [DataMember]
+        public int Number1 { get; set; }
+        [DataMember]
+        public int Number2 { get; set; }
+        //bool boolValue = true;
+        //string stringValue = "Hello ";
+
+        //[DataMember]
+        //public bool BoolValue
+        //{
+        //    get { return boolValue; }
+        //    set { boolValue = value; }
+        //}
+
+        //[DataMember]
+        //public string StringValue
+        //{
+        //    get { return stringValue; }
+        //    set { stringValue = value; }
+        //}
+    }
+}
